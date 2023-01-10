bcff8a91b9814149489fcfcdd6b6f0d4b4df1e17
DDomix <dominik.papp1010@gmail.com>
2023-01-10 13:42:47 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { ChangeEvent, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+interface State{
+  datum: Date;
+  dobas: number;
+  meret: number;
+}
+class App extends Component<{},State>{
+
+  constructor(props: {}){
+    super(props);
+    this.state={
+      datum: new Date(),
+      dobas: 0,
+      meret: 12,
+    }
+    setInterval(() =>{
+      this.setState({
+        datum:new Date()
+      });
+    },1000);
+  }
+
+  kockadobas=()=>{
+    const ujSzam=Math.floor(Math.random()*6+1);
+    this.setState({
+      dobas: ujSzam
+    })
+  }
+
+  betumeret=(e: ChangeEvent /* InputEvent */ )=>{
+    let ujMeret=parseInt((e.currentTarget as HTMLInputElement).value);
+    this.setState({
+      meret: ujMeret,
+    })
+  }
+  
+  render() {
+    return <div style={{fontSize: this.state.meret + 'pt'}}>
+      <p id="hello" style={{
+        fontStyle: 'italic',
+        color: 'aqua'
+      }}>
+        {this.state.datum.toLocaleString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p className="kockadobas">Kockadobás eredménye: {this.state.dobas}</p>
+        <button onClick={this.kockadobas}>Kockadobás</button>
+        <input type="number" onChange={this.betumeret}></input>
+    </div>;
+  }
 }
 
 export default App;
